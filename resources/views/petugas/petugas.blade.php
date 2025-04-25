39ea8a58b38da4acae1a38810932a298a86b6c73
PutiBungsu <it.pbungsu@gmail.com>
2025-04-25 15:12:21 +0700

    feat: buat tabel daftar pasien, masih eksperimen overflow

---
+++ resources/views/petugas/petugas.blade.php
@@ -0,0 +1,75 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl leading-tight font-semibold text-gray-800">{{ __('Petugas') }}</h2>
+    </x-slot>
+
+    <div class="py-12 px-6">
+        <div class="max-w-7xl xl:max-w-[1920px] mx-auto px-2 sm:px-2 lg:px-4">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg pt-6 px-6 ">
+                <div class="text-gray-900 block w-full overflow-x-auto">
+                    <div class="flex flex-row w-full py-2 justify-between">
+                        <div class="font-semibold text-4xl text-gray-800 leading-tight">Data Petugas</div>
+                        <div class="flex items-center">
+                            <label for="first_name"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900 mr-2 pt-2">Cari</label>
+                            <input type="text" id="first_name"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-slate-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-8"
+                                placeholder="Nomor Rekam Medis" required />
+                        </div>
+                    </div>
+                </div>
+                <form action="{{ route('input-petugas') }}" method="GET">
+                    <button id="btnInputPetugas" type="submit"
+                        class="float-right mx-8 my-0.5 rounded-lg border-x-orange-900 bg-purple-600 px-4 py-3 text-sm font-black text-white hover:bg-purple-700 focus:ring-4 focus:ring-purple-300 focus:outline-none dark:bg-purple-600 dark:text-white dark:hover:bg-purple-700 dark:focus:ring-purple-900"><i
+                            class="bi bi-plus-circle"></i> Tambah</button>
+                </form>
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-100 uppercase bg-gray-700 dark:bg-gray-700 dark:text-gray-100">
+                        <tr>
+                            <th scope="col" class="px-4 py-3" onclick="sortTable(0, this)">
+                                NIP <span class="sort-icon cursor-pointer sort-header"><i
+                                        class="bi bi-chevron-expand"></i></span>
+                            </th>
+                            <th scope="col" class="px-4 py-3" onclick="sortTable(1, this)">
+                                Nama Petugas <span class="sort-icon cursor-pointer sort-header"><i
+                                        class="bi bi-chevron-expand"></i></span>
+                            </th>
+                            <th scope="col" class="px-4 py-3" onclick="sortTable(2, this)">
+                                Email <span class="sort-icon cursor-pointer sort-header"><i
+                                        class="bi bi-chevron-expand"></i></span>
+                            </th>
+                            <th scope="col" class="px-4 py-3" onclick="sortTable(3, this)">
+                                Unit <span class="sort-icon cursor-pointer sort-header"><i
+                                        class="bi bi-chevron-expand"></i></span>
+                            </th>
+                            <th scope="col" class="px-4 py-3" onclick="sortTable(3, this)">
+                                Status <span class="sort-icon cursor-pointer sort-header"><i
+                                        class="bi bi-chevron-expand"></i></span>
+                            </th>
+                            <th scope="col" class="px-4 py-3" onclick="sortTable(3, this)">
+                                Role <span class="sort-icon cursor-pointer sort-header"><i
+                                        class="bi bi-chevron-expand"></i></span>
+                            </th>
+                            <th scope="col" class="px-4 py-3"></th> <!-- For actions -->
+                        </tr>
+                    </thead>
+                    <tbody class="max-h-[400px] overflow-y-auto">
+                        @for ($i = 0; $i < 100; $i++) <tr
+                            class="border-b dark:border-gray-700 text-black odd:bg-white odd:dark:bg-white even:bg-gray-100 even:dark:bg-gray-100">
+                            <td class="px-4">123456{{ $i }}</td>
+                            <td class="px-4">Nama Petugas {{ $i }}</td>
+                            <td class="px-4">email{{ $i }}@example.com</td>
+                            <td class="px-4">Unit {{ $i }}</td>
+                            <td class="px-4">Status {{ $i }}</td>
+                            <td class="px-4">Role {{ $i }}</td>
+                            <td class="px-4 pt-2 flex justify-end items-center gap-2">
+                                <!-- Tombol Edit dan Delete -->
+                            </td>
+                            </tr>
+                            @endfor
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
